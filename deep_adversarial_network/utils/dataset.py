ec3d8dc4cde0580cdaecf4dd1a1f793a1716648b
Shivangi Aneja <shivangi.aneja@tum.de>
2018-12-05 23:50:04 +0100

    overfit 5 images

--- deep_adversarial_network/utils/dataset.py
+++ deep_adversarial_network/utils/dataset.py
@@ -88,7 +88,7 @@ class TOY_DATA(BaseDataset):
         image_tuple = np.load(dirpath + '/toy_data.npy')
 
         train_data = torch.stack([torch.Tensor(i) for i in image_tuple[0:5]])
-        val_data = torch.stack([torch.Tensor(i) for i in image_tuple[5:7]])
+        val_data = torch.stack([torch.Tensor(i) for i in image_tuple[16:18]])
 
         # train = CustomDataset1(comp_image=train_data[:,0,:,:], fg_img=train_data[:,1,:,:],
         #                       alpha=train_data[:,2,:,:], bg_img=train_data[:,3,:,:])

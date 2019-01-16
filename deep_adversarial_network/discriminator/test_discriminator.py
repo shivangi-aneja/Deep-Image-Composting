4c52549fa98e93dbe699f61ff93d4e3d1cd55760
SohamMazumder <sohammazumder95@gmail.com>
2019-01-16 17:21:49 +0100

    add resnet discriminator

--- deep_adversarial_network/discriminator/test_discriminator.py
+++ deep_adversarial_network/discriminator/test_discriminator.py
@@ -69,34 +69,58 @@ class test_Discriminator2(object):
         return out, logits
 
 
-class test_Discriminator3(object):
+class Resnet_Discriminator(object):
     """
     test_Discriminator1
     """
     def __init__(self):
         pass
 
-    def make_discriminator_network(self, x, reuse=False,isTrain=True):
+    def make_discriminator_network(self, x, reuse=False, isTrain=True):
         with tf.variable_scope("discriminator", reuse=reuse):
             #input = tf.placeholder(tf.float32, (None, 16, 16, 3), name="input")
-            conv1 = tf.layers.conv2d(inputs=x, filters=64, kernel_size=(5,5), padding='valid', strides=2,
-                                     activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
-            conv1_bn = tf.layers.batch_normalization(conv1)
+            conv1 = tf.layers.conv2d(inputs=x, filters=64, kernel_size=(7, 7), padding='valid', strides=2,
+                                     activation=None, kernel_initializer=tf.contrib.layers.xavier_initializer())
+            conv1_maxpool = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
-            conv2 = tf.layers.conv2d(inputs=conv1_bn, filters=128, kernel_size=(5, 5), padding='valid', strides=2,
-                                     activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
-            conv2_bn = tf.layers.batch_normalization(conv2)
+            #Block1
 
-            conv3 = tf.layers.conv2d(inputs=conv2_bn, filters=256, kernel_size=(5, 5), padding='same', strides=2,
-                                     activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
-            conv3_bn = tf.layers.batch_normalization(conv3)
+            resnet_conv1 = tf.layers.conv2d(inputs=conv1_maxpool, filters=64, kernel_size=(3, 3), padding='valid', strides=2,
+                                     activation=None, kernel_initializer=tf.contrib.layers.xavier_initializer())
 
-            conv4 = tf.layers.conv2d(inputs=conv3_bn, filters=512, kernel_size=(5, 5), padding='same', strides=2,
-                                     activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
+            resnet_conv1_bn = tf.layers.batch_normalization(resnet_conv1)
+            resnet_conv1_bn = tf.nn.relu(resnet_conv1_bn)
 
-            fc4_reshape = tf.reshape(conv4, shape=[-1, 2048])
+            resnet_conv2 = tf.layers.conv2d(inputs=resnet_conv1_bn, filters=64, kernel_size=(3, 3), padding='valid',
+                                            strides=2,activation=None, kernel_initializer=tf.contrib.layers.xavier_initializer())
 
-            logits = tf.layers.dense(fc4_reshape, units=1)
+            resnet_conv2_bn = tf.layers.batch_normalization(resnet_conv2)
+            resnet_conv2_bn = tf.nn.relu(resnet_conv2_bn)
+
+            resnet_conv2_bn += conv1_maxpool
+            resnet_conv2_bn = tf.nn.relu(resnet_conv2_bn)
+
+            #Block2
+
+            resnet2_conv1 = tf.layers.conv2d(inputs=resnet_conv2_bn, filters=64, kernel_size=(3, 3), padding='valid',
+                                            strides=2, activation=None, kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+            resnet2_conv1_bn = tf.layers.batch_normalization(resnet2_conv1)
+            resnet2_conv1_bn = tf.nn.relu(resnet2_conv1_bn)
+
+            resnet2_conv2 = tf.layers.conv2d(inputs=resnet2_conv1_bn, filters=64, kernel_size=(3, 3), padding='valid',
+                                            strides=2, activation=None, kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+            resnet2_conv2_bn = tf.layers.batch_normalization(resnet2_conv2)
+            resnet2_conv2_bn = tf.nn.relu(resnet2_conv2_bn)
+
+            resnet2_conv2_bn += resnet_conv2_bn
+            resnet2_conv2_bn = tf.nn.relu(resnet2_conv2_bn)
+
+
+            reshape = tf.reshape(resnet2_conv2_bn, shape=[-1, 2048])
+
+            logits = tf.layers.dense(reshape, units=1)
             out = tf.nn.sigmoid(logits)
 
         return out, logits

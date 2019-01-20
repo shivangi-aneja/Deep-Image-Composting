1ba854e1277c04cab030c84246f454b331239f2e
Shivangi Aneja <shivangi.aneja@tum.de>
2019-01-20 14:28:18 +0100

    code refactor

--- deep_adversarial_network/adversarial_training.py
+++ deep_adversarial_network/adversarial_training.py
@@ -4,7 +4,7 @@ import time
 
 import matplotlib.pyplot as plt
 import numpy as np
-from deep_adversarial_network.custom_vgg16 import *
+from deep_adversarial_network.losses.custom_losses import perceptual_loss
 from deep_adversarial_network.logging.logger import rootLogger
 from deep_adversarial_network.logging.tf_logger import Logger
 from deep_adversarial_network.metrics.metric_eval import (calc_mse_psnr)
@@ -44,24 +44,10 @@ class DeepGAN(object):
         self.batch_size = batch_size
         self.mplib = mplib
         self.sess = None
+        self.perceptual_loss = perceptual_loss
         # Tensorboard Logging
         self.logger = Logger(model_name=self.model_name, data_name=self.dataset, log_path=tf_log_path)
 
-    def perceptual_loss(self, inputs, outputs):
-        direc = os.getcwd()
-        data_dict = loadWeightsData(direc + '/deep_adversarial_network/vgg16.npy')
-        
-        vgg_c = custom_Vgg16(inputs, data_dict=data_dict)
-        feature_ = [vgg_c.conv1_2, vgg_c.conv2_2, vgg_c.conv3_3, vgg_c.conv4_3, vgg_c.conv5_3]
-
-        vgg = custom_Vgg16(outputs, data_dict=data_dict)
-        feature = [vgg.conv1_2, vgg.conv2_2, vgg.conv3_3, vgg.conv4_3, vgg.conv5_3]
-
-        loss_f = tf.zeros(self.batch_size, tf.float32)
-        for f, f_ in zip(feature, feature_):
-            loss_f += tf.reduce_mean(tf.subtract(f, f_) ** 2, [1, 2, 3])
-
-        return loss_f
 
     def adversarial_train(self, train_loader, test_loader, model_path):
         """
@@ -98,7 +84,7 @@ class DeepGAN(object):
         G_loss1 = tf.reduce_mean(self.recon_loss(gt_img, G_z))
         G_loss2 = tf.reduce_mean(
             tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.zeros_like(D_fake_logits)))
-        G_perceptual_loss = self.perceptual_loss(G_z, gt_img)
+        G_perceptual_loss = self.perceptual_loss(self.batch_size, G_z, gt_img)
         G_loss = G_loss2 + 0 * G_loss1 + 0.1*G_perceptual_loss
 
         # trainable variables for each network
---
+++ deep_adversarial_network/losses/custom_losses.py
@@ -0,0 +1,25 @@
+from deep_adversarial_network.losses.custom_vgg16 import *
+
+
+def perceptual_loss(batch_size, inputs, outputs):
+    """
+    Perceptual Loss
+    :param batch_size:
+    :param inputs:
+    :param outputs:
+    :return:
+    """
+    direc = os.getcwd()
+    data_dict = loadWeightsData(direc + '/deep_adversarial_network/weights/vgg16.npy')
+
+    vgg_c = custom_Vgg16(inputs, data_dict=data_dict)
+    feature_ = [vgg_c.conv1_2, vgg_c.conv2_2, vgg_c.conv3_3, vgg_c.conv4_3, vgg_c.conv5_3]
+
+    vgg = custom_Vgg16(outputs, data_dict=data_dict)
+    feature = [vgg.conv1_2, vgg.conv2_2, vgg.conv3_3, vgg.conv4_3, vgg.conv5_3]
+
+    loss_f = tf.zeros(batch_size, tf.float32)
+    for f, f_ in zip(feature, feature_):
+        loss_f += tf.reduce_mean(tf.subtract(f, f_) ** 2, [1, 2, 3])
+
+    return loss_f
